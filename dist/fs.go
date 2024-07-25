e7c21b44cb8a8a58eb6a863c30d7805de42fdaf7
sarumaj <sarumaj@users.noreply.github.com>
2024-07-25 13:41:36 +0200

    move info box around

--- dist/fs.go
+++ dist/fs.go
@@ -1,4 +1,4 @@
-// Code generated on 2024-07-24T22:00:06.789Z+00:00, DO NOT EDIT.
+// Code generated on 2024-07-25T11:41:24.711Z+00:00, DO NOT EDIT.
 package dist
 
 import (
@@ -105,7 +105,7 @@ func (h httpFS) Open(name string) (http.File, error) {
 	return nil, fs.ErrNotExist
 }
 
-func BuildTime() string { return "2024-07-24T22:00:06.789Z+00:00" }
+func BuildTime() string { return "2024-07-25T11:41:24.711Z+00:00" }
 
 func LookupHash(name string) (string, bool) {
 	hash, ok := hashMap[name]
